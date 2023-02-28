b2b6b19c79d4d6e655dd30277fea21150d1e1813
wesley-nunes-00@hotmail.com <wesley-nunes-00@hotmail.com>
2023-02-28 19:32:03 -0300

    remoção do operador if e chamada do tostring formatado

--- src/Entities/Registry.java
+++ src/Entities/Registry.java
@@ -11,86 +11,56 @@ public class Registry {
 	private Date hora_entrada;
 	private Date hora_saida;
 	private double valorTotal;
-	
-	
+
 	public Registry() {
 
 	}
-	
-	
-	
-	
+
 	public Registry(String placa, Date hora_entrada) {
 		this.placa = placa;
 		this.hora_entrada = hora_entrada;
 	}
 
-
-
-
 	public String getPlaca() {
 		return placa;
 	}
-
-
-
 
 	public void setPlaca(String placa) {
 		this.placa = placa;
 	}
 
-
-
-
 	public Date getHora_entrada() {
 		return hora_entrada;
 	}
-
-
-
 
 	public void setHora_entrada(Date hora_entrada) {
 		this.hora_entrada = hora_entrada;
 	}
 
-
-
-
 	public Date getHora_saida() {
 		return hora_saida;
 	}
-
-
-
 
 	public void setHora_saida(Date hora_saida) {
 		this.hora_saida = hora_saida;
 	}
 
-
-
-
 	public double getValorTotal() {
 		return valorTotal;
 	}
-
-
-
 
 	public void setValorTotal(double valorTotal) {
 		this.valorTotal = valorTotal;
 	}
 
-
-
-
 	public void calcularValor() {
-		 long hora_saida = this.hora_saida.getTime();
-		 long hora_entrada = this.hora_entrada.getTime();
-		 long dif = (hora_saida - hora_entrada) / 1000;
-		 this.valorTotal =  (dif /60) * 0.10;
+		long hora_saida = this.hora_saida.getTime();
+		long hora_entrada = this.hora_entrada.getTime();
+		long dif = (hora_saida - hora_entrada) / 1000;
+		dif = dif / 60;
+		this.valorTotal = (dif * 0.10);
+
 	}
-	
 
 	@Override
 	public String toString() {
@@ -100,13 +70,11 @@ public class Registry {
 		dfs.setDecimalSeparator('.');
 		String padraoBR = "###,###.##";
 		DecimalFormat df = new DecimalFormat(padraoBR, dfs);
-		
-		return 	  "\n ---------------------------------------------------"
-				+((placa != null) ? "\n Placa: " + placa : "") 
-				+ ((hora_entrada !=null) ? "\n Horario de entrada: " + sdf.format(hora_entrada) : "")
+
+		return "\n ---------------------------------------------------" + ((placa != null) ? "\n Placa: " + placa : "")
+				+ ((hora_entrada != null) ? "\n Horario de entrada: " + sdf.format(hora_entrada) : "")
 				+ ((hora_saida != null) ? "\n Horario de saida: " + sdf.format(hora_saida) : "")
 				+ ((valorTotal != 0.00) ? "\n Valor total: R$ " + df.format(valorTotal) : "")
-				+ "\n ---------------------------------------------------"
-				;
+				+ "\n ---------------------------------------------------";
 	}
 }
--- src/Program/Main.java
+++ src/Program/Main.java
@@ -11,84 +11,71 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		ArrayList<Registry> lista = new ArrayList<>();
-		
+
 		while (true) {
-		
-		System.out.println("1- Adicionar");
-		System.out.println("2- Retirar");
-		System.out.println("3- Lista");
-		int opcao = sc.nextInt();
-		
-		switch (opcao) {
-		case 1:
-			System.out.print("Informe a placa: ");
-			sc.nextLine();
-			String placa = sc.nextLine();
-			System.out.println();
-			Registry registry = new Registry(placa, new Date());
-			lista.add(registry);
-			
-			break;
-			
-		case 2:
-			System.out.print("Informe a placa do veiculo para retirada: ");
-			sc.nextLine();
-			String buscaPlaca = sc.nextLine();
-			Registry reg = null;
-			
-			
-			
-			for(Registry registro : lista) {
-				if(registro.getPlaca().equals(buscaPlaca)) {
-					reg = registro;
+
+			System.out.println("1- Adicionar");
+			System.out.println("2- Retirar");
+			System.out.println("3- Lista");
+			int opcao = sc.nextInt();
+
+			switch (opcao) {
+			case 1:
+				System.out.print("Informe a placa: ");
+				sc.nextLine();
+				String placa = sc.nextLine();
+				System.out.println();
+				Registry registry = new Registry(placa, new Date());
+				lista.add(registry);
+
+				break;
+
+			case 2:
+				System.out.print("Informe a placa do veiculo para retirada: ");
+				sc.nextLine();
+				String buscaPlaca = sc.nextLine();
+				Registry reg = null;
+
+				for (Registry registro : lista) {
+					if (registro.getPlaca().equals(buscaPlaca)) {
+						reg = registro;
+					}
 				}
-			}
-			
-			if(reg == null) {
-				System.err.println("Registro nao encontrado!!!");
-	
-			}else {
-				
+
+				if (reg == null) {
+					System.err.println("Registro nao encontrado!!!");
+
+				} else {
+
 					reg.setHora_saida(new Date());
 					reg.calcularValor();
-					
-					System.out.println(reg.toString());
-					
-					//System.out.println("Confirma retirada? (y/n)");
-					
-					/* 
-					 * String resp = sc.nextLine();
-					 * 
-					 * if (resp == "y") { lista.remove(reg); System.out.println("removed!"); }
-					 */
-					
-					//lista.remove(reg);
-			
-			}
-			
-			
-			break;
-			
-		case 3:
-			
-			for(Registry registro : lista) {
-				System.out.println(registro);
-			}
-			
-			break;	
 
-		default:
-			System.err.println("Opção invalida!!!");
-			
-			break;
+					System.out.println(reg.toString());
+
+					lista.remove(reg);
+
+				}
+
+				break;
+
+			case 3:
+
+				for (Registry registro : lista) {
+					System.out.println(registro);
+				}
+
+				break;
+
+			default:
+				System.err.println("Opção invalida!!!");
+
+				break;
 			}
 		}
 	}
-	
-}	
-	/*remover buffer
-	 private static void clearBuffer(Scanner scanner) {
-	        if (scanner.hasNextLine()) {
-	            scanner.nextLine();
-	        }
-	    }*/
+
+}
+/*
+ * remover buffer private static void clearBuffer(Scanner scanner) { if
+ * (scanner.hasNextLine()) { scanner.nextLine(); } }
+ */
