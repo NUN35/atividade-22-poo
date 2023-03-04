0cbe95744b0e21a16bb8fa46205cee6d5f254e03
Aleff <aleff.remberto@hotmail.com>
2023-03-04 17:39:09 -0300

    refatoração de código

--- src/Entities/Registry.java
+++ src/Entities/Registry.java
@@ -1,80 +1,75 @@
 package Entities;
 
-import java.text.DecimalFormat;
-import java.text.DecimalFormatSymbols;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.Locale;
 
 public class Registry {
-	private String placa;
-	private Date hora_entrada;
-	private Date hora_saida;
-	private double valorTotal;
+    private String placa;
+    private Date hora_entrada;
+    private Date hora_saida;
+    private double valorTotal;
 
-	public Registry() {
+    public Registry() {
 
-	}
+    }
 
-	public Registry(String placa, Date hora_entrada) {
-		this.placa = placa;
-		this.hora_entrada = hora_entrada;
-	}
+    public Registry(String placa, Date hora_entrada) {
+        long time = hora_entrada.getTime();
+        Date date = new Date(time - 3600000);
+        this.placa = placa;
+        this.hora_entrada = date;
+    }
 
-	public String getPlaca() {
-		return placa;
-	}
+    public String getPlaca() {
+        return placa;
+    }
 
-	public void setPlaca(String placa) {
-		this.placa = placa;
-	}
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
 
-	public Date getHora_entrada() {
-		return hora_entrada;
-	}
+    public Date getHora_entrada() {
+        return hora_entrada;
+    }
 
-	public void setHora_entrada(Date hora_entrada) {
-		this.hora_entrada = hora_entrada;
-	}
+    public void setHora_entrada(Date hora_entrada) {
+        this.hora_entrada = hora_entrada;
+    }
 
-	public Date getHora_saida() {
-		return hora_saida;
-	}
+    public Date getHora_saida() {
+        return hora_saida;
+    }
 
-	public void setHora_saida(Date hora_saida) {
-		this.hora_saida = hora_saida;
-	}
+    public void setHora_saida(Date hora_saida) {
+        this.hora_saida = hora_saida;
+    }
 
-	public double getValorTotal() {
-		return valorTotal;
-	}
+    public double getValorTotal() {
+        return valorTotal;
+    }
 
-	public void setValorTotal(double valorTotal) {
-		this.valorTotal = valorTotal;
-	}
+    public void setValorTotal(double valorTotal) {
+        this.valorTotal = valorTotal;
+    }
 
-	public void calcularValor() {
-		long hora_saida = this.hora_saida.getTime();
-		long hora_entrada = this.hora_entrada.getTime();
-		long dif = (hora_saida - hora_entrada) / 1000;
-		dif = dif / 60;
-		this.valorTotal = (dif * 0.10);
+    /**
+     * Calcula o valor total referente a 10 centavos por minuto;
+     */
+    public void calcularValor() {
+        long hora_saida = this.hora_saida.getTime();
+        long hora_entrada = this.hora_entrada.getTime();
+        long dif = (hora_saida - hora_entrada) / 1000;
+        dif = dif / 60 / 60;
+        this.valorTotal = (dif * 15);
+    }
 
-	}
-
-	@Override
-	public String toString() {
-		SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss - dd/MMM/yyyy");
-		DecimalFormatSymbols dfs = new DecimalFormatSymbols(new Locale("pt", "Brazil"));
-		dfs.setDecimalSeparator(',');
-		dfs.setDecimalSeparator('.');
-		String padraoBR = "###,###.##";
-		DecimalFormat df = new DecimalFormat(padraoBR, dfs);
-
-		return "\n ---------------------------------------------------" + ((placa != null) ? "\n Placa: " + placa : "")
-				+ ((hora_entrada != null) ? "\n Horario de entrada: " + sdf.format(hora_entrada) : "")
-				+ ((hora_saida != null) ? "\n Horario de saida: " + sdf.format(hora_saida) : "")
-				+ ((valorTotal != 0.00) ? "\n Valor total: R$ " + df.format(valorTotal) : "")
-				+ "\n ---------------------------------------------------";
-	}
+    @Override
+    public String toString() {
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss - dd/MMMM/yyyy");
+        return "\n ---------------------------------------------------" + ((placa != null) ? "\n Placa: " + placa : "")
+                + ((hora_entrada != null) ? "\n Horario de entrada: " + sdf.format(hora_entrada) : "")
+                + ((hora_saida != null) ? "\n Horario de saida: " + sdf.format(hora_saida) : "")
+                + ((valorTotal != 0.00) ? "\n Valor total: R$ " + String.format("%.2f", valorTotal) : "")
+                + "\n ---------------------------------------------------";
+    }
 }
--- src/Program/Main.java
+++ src/Program/Main.java
@@ -1,81 +1,114 @@
 package Program;
 
+import Entities.Registry;
+
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.Scanner;
 
-import Entities.Registry;
-
 public class Main {
+    private ArrayList<Registry> lista = new ArrayList<>();
+    private final Scanner sc = new Scanner(System.in);
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		ArrayList<Registry> lista = new ArrayList<>();
 
-		while (true) {
+    public static void main(String[] args) {
+        System.out.println("Iniciando o software...");
+        boolean exec = true;
+        Main main = new Main();
+        main.selecionarOpcao(exec, main);
+    }
 
-			System.out.println("1- Adicionar");
-			System.out.println("2- Retirar");
-			System.out.println("3- Lista");
-			int opcao = sc.nextInt();
+    private void selecionarOpcao(boolean exec, Main main) {
+        while (exec) {
+            main.mostrarMenu();
+            int opcao = this.sc.nextInt();
 
-			switch (opcao) {
-			case 1:
-				System.out.print("Informe a placa: ");
-				sc.nextLine();
-				String placa = sc.nextLine();
-				System.out.println();
-				Registry registry = new Registry(placa, new Date());
-				lista.add(registry);
+            switch (opcao) {
+                case 1:
+                    main.adicionarVeiculo();
+                    break;
 
-				break;
+                case 2:
+                    main.removerVeiculo();
+                    break;
 
-			case 2:
-				System.out.print("Informe a placa do veiculo para retirada: ");
-				sc.nextLine();
-				String buscaPlaca = sc.nextLine();
-				Registry reg = null;
+                case 3:
+                    main.listarVeiculo();
+                    break;
 
-				for (Registry registro : lista) {
-					if (registro.getPlaca().equals(buscaPlaca)) {
-						reg = registro;
-					}
-				}
+                case 4:
+                    exec = false;
+                    System.out.println("Programa fechando...");
+                    break;
 
-				if (reg == null) {
-					System.err.println("Registro nao encontrado!!!");
+                default:
+                    System.err.println("Opção invalida!!!");
+                    break;
+            }
+        }
+    }
 
-				} else {
+    private void mostrarMenu() {
+        System.out.println("1 - Adicionar");
+        System.out.println("2 - Retirar");
+        System.out.println("3 - Lista");
+        System.out.println("4 - Fechar Programa");
+    }
 
-					reg.setHora_saida(new Date());
-					reg.calcularValor();
+    private void listarVeiculo() {
+        for (Registry registro : this.lista) {
+            System.out.println(registro);
+        }
+    }
 
-					System.out.println(reg.toString());
+    private void removerVeiculo() {
+        listarVeiculo();
+        System.out.print("Informe a placa do veiculo para retirada: ");
+        String buscaPlaca = sc.nextLine();
+        Registry reg = null;
 
-					lista.remove(reg);
+        reg = verificaSeJaExisteNoEstacionamento(buscaPlaca);
 
-				}
+        if (reg == null) {
+            System.err.println("Registro nao encontrado!!!");
+        } else {
+            System.out.println("Deseja remover o veiculo do estacionamento? 1 - sim / 2 - não");
+            int confirmacao = sc.nextInt();
 
-				break;
+            if (confirmacao == 1) {
+                reg.setHora_saida(new Date());
+                reg.calcularValor();
+                System.out.println(reg);
+                lista.remove(reg);
+            } else if (confirmacao == 2) {
+                return;
+            } else {
+                System.out.println("Opção invalidda");
+            }
+        }
+    }
 
-			case 3:
+    private void adicionarVeiculo() {
+        System.out.print("Informe a placa: ");
+        String placa = sc.nextLine();
+        Registry registry = new Registry(placa, new Date());
 
-				for (Registry registro : lista) {
-					System.out.println(registro);
-				}
+        Registry registrado = verificaSeJaExisteNoEstacionamento(placa);
 
-				break;
+        if (registrado == null) {
+            this.lista.add(registry);
+        } else {
+            System.err.println("Placa ja consta no sistema!");
+        }
+    }
 
-			default:
-				System.err.println("Opção invalida!!!");
-
-				break;
-			}
-		}
-	}
+    private Registry verificaSeJaExisteNoEstacionamento(String placa) {
+        for (Registry reg : this.lista) {
+            if (reg.getPlaca().equalsIgnoreCase(placa)) {
+                return reg;
+            }
+        }
+        return null;
+    }
 
 }
-/*
- * remover buffer private static void clearBuffer(Scanner scanner) { if
- * (scanner.hasNextLine()) { scanner.nextLine(); } }
- */
